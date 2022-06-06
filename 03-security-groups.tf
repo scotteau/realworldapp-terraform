6e4325c7b4224e4923d7c5d227b91e2d6478716a
scottw9812 <dev@scottw.xyz>
2022-06-06 15:56:32 +0800

    added descriptions for special access sg rules

--- 03-security-groups.tf
+++ 03-security-groups.tf
@@ -86,6 +86,7 @@ variable "your_cidr" {
 }
 
 resource "aws_security_group_rule" "initial_access" {
+  description       = "Grant access to local machine to rds sg"
   type              = "ingress"
   cidr_blocks       = var.your_cidr
   from_port         = 5432
@@ -95,12 +96,13 @@ resource "aws_security_group_rule" "initial_access" {
 }
 
 resource "aws_security_group_rule" "access" {
+  description       = "Grant access to local machine to ecs sg"
   from_port         = 3000
   protocol          = "tcp"
   security_group_id = aws_security_group.ecs.id
   to_port           = 3000
   type              = "ingress"
-  cidr_blocks = var.your_cidr
+  cidr_blocks       = var.your_cidr
 }
 
 
