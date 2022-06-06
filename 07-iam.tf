6fe7c312b0ec43e90f9ca213712d7237517c0a34
scottw9812 <dev@scottw.xyz>
2022-06-06 12:27:17 +0800

    added ecs task definition

--- 07-iam.tf
+++ 07-iam.tf
@@ -1,15 +1,15 @@
-################################################################################
-# ecs_task_execution_role
-#################################################################################
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name               = "ecs_task_execution_role"
-  assume_role_policy = file("${path.root}/templates/ecs_task_execution_role.json")
+data "aws_iam_policy_document" "ecs_tasks_execution_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
 }
 
-################################################################################
-# ecs_task_role
-#################################################################################
-resource "aws_iam_role" "ecs_task_role" {
-  name               = "ecs_task_role"
-  assume_role_policy = file("${path.root}/templates/ecs_task_role_rds_access.json")
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "ecs_task_execution_role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_tasks_execution_role.json
 }
