4c9dd9f8382a7accf7386ee313a15614e98d6646
scottw9812 <dev@scottw.xyz>
2022-06-06 15:56:55 +0800

    fix issue with ecs execution roles

--- 07-iam.tf
+++ 07-iam.tf
@@ -13,3 +13,9 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   name               = "ecs_task_execution_role"
   assume_role_policy = data.aws_iam_policy_document.ecs_tasks_execution_role.json
 }
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
+
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  role       = aws_iam_role.ecs_task_execution_role.name
+}
