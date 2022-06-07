cfed2a58811f05d4d35980af18453a57464e2cdb
scottw9812 <dev@scottw.xyz>
2022-06-07 18:43:12 +0800

    added domain dns to cloudfront distribution

---
+++ 09-route53.tf
@@ -0,0 +1,36 @@
+data "aws_route53_zone" "domain" {
+  name = var.domain_name
+}
+
+resource "aws_route53_record" "root" {
+  name    = var.domain_name
+  type    = "A"
+  zone_id = data.aws_route53_zone.domain.zone_id
+
+  alias {
+    evaluate_target_health = false
+    name                   = aws_cloudfront_distribution.root_s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.root_s3_distribution.hosted_zone_id
+  }
+}
+
+resource "aws_route53_record" "www" {
+
+  name    = "www.${var.domain_name}"
+  type    = "A"
+  zone_id = data.aws_route53_zone.domain.zone_id
+
+  alias {
+    evaluate_target_health = false
+    name                   = aws_cloudfront_distribution.root_s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.root_s3_distribution.hosted_zone_id
+  }
+}
+
+
+
+
+
+
+
+
